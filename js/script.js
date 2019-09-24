7b812aa5ff1565180d81a6276fbe143e78244969
Никита <kondor12371@yandex.ru>
2019-09-24 14:56:54 +0300

    Исправил пару багов.

--- js/script.js
+++ js/script.js
@@ -2,12 +2,14 @@ let promoSlides = document.querySelectorAll('.promo-slider__slide');
 let promoLeftArrow = document.querySelector('.promo-slider__togle-left');
 let promoRightArrow = document.querySelector('.promo-slider__togle-right');
 let current = 0;
+let priceListCurrent = 0;
+let staffCurrent = 0;
 
-function slider() {
-  for (let i = 0; i<promoSlides.length; i++) {
-    promoSlides[i].classList.add('shide');
+function slider(slides, counter) {
+  for (let i = 0; i<slides.length; i++) {
+    slides[i].classList.add('shide');
   }
-  promoSlides[current].classList.remove('shide');
+  slides[counter].classList.remove('shide');
 
 }
 
@@ -18,7 +20,7 @@ promoLeftArrow.addEventListener('click', function(){
   else {
     current--;
   }
-  slider();
+  slider(promoSlides, current);
 })
 
 promoRightArrow.addEventListener('click', function(){
@@ -28,13 +30,68 @@ promoRightArrow.addEventListener('click', function(){
   else {
     current++;
   }
-  slider();
+  slider(promoSlides, current);
 })
 
 let additionalText = document.querySelector('.about-us__additional-text');
 let buttonMore = document.querySelector('.button-more');
+let textImage = document.querySelector('.about-us__image')
 
 buttonMore.addEventListener('click', function(){
   additionalText.classList.remove('shide');
   buttonMore.classList.add('shide');
+  textImage.classList.add('about-us__add-image');
+})
+
+let priceListButtonLeft = document.querySelector('.price-list__button-left');
+let priceListButtonRight = document.querySelector('.price-list__button-right');
+let priceListSlides = document.querySelectorAll('.price-list__slide');
+
+priceListButtonLeft.addEventListener('click', function(evt){
+  evt.preventDefault();
+  if(priceListCurrent - 1 == -1) {
+    current = priceListSlides.length-1;
+  }
+  else {
+    priceListCurrent--;
+  }
+  slider(priceListSlides, priceListCurrent);
+})
+
+priceListButtonRight.addEventListener('click', function(evt){
+  evt.preventDefault();
+  if(current + 1 == priceListSlides.length) {
+    priceListCurrent = 0;
+  }
+  else {
+    priceListCurrent++;
+  }
+  slider(priceListSlides, priceListCurrent);
+})
+
+let staffButtonLeft = document.querySelector('.our-staff__left-arrow');
+let staffButtonRight = document.querySelector('.our-staff__right-arrow');
+let staffSlides = document.querySelectorAll('.staff-slider__slide');
+
+
+staffButtonLeft.addEventListener('click', function(evt){
+  evt.preventDefault();
+  if(staffCurrent - 1 == -1) {
+    staffCurrent = staffSlides.length-1;
+  }
+  else {
+    staffCurrent--;
+  }
+  slider(staffSlides, staffCurrent);
+})
+
+staffButtonRight.addEventListener('click', function(evt){
+  evt.preventDefault();
+  if(staffCurrent + 1 == staffSlides.length) {
+    staffCurrent = 0;
+  }
+  else {
+    staffCurrent++;
+  }
+  slider(staffSlides, staffCurrent);
 })
